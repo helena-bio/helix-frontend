c6e4bd866b01d326c1b2ac0c8a5dd8094d77abe8
Vladimir Mitev <dev@helena.bio>
2026-01-21 20:39:07 +0200

    fix(helix-loader): reduce xs size from 48x70 to 40x58

--- app/components/ui/helix-loader.tsx
+++ app/components/ui/helix-loader.tsx
@@ -10,7 +10,7 @@ interface HelixLoaderProps {
 }
 
 const sizeMap = {
-  xs: { width: 48, height: 70 },
+  xs: { width: 40, height: 58 },
   sm: { width: 64, height: 93 },
   md: { width: 96, height: 140 },
   lg: { width: 144, height: 210 }
