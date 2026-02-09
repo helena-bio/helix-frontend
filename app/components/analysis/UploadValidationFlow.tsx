7b43684620fa6d1f0c774a03200a1825dd1f5946
Vladimir Mitev <dev@helena.bio>
2026-02-09 12:42:30 +0200

    Remove gap override on QC results card

--- app/components/analysis/UploadValidationFlow.tsx
+++ app/components/analysis/UploadValidationFlow.tsx
@@ -519,7 +519,7 @@ export function UploadValidationFlow({ onComplete, onError }: UploadValidationFl
           </div>
 
           {/* File Info & QC Results Card */}
-          <Card>
+          <Card className="gap-0">
             <CardContent className="p-6">
               {/* File Header */}
               <div className="border-b border-border pb-4 mb-4">
